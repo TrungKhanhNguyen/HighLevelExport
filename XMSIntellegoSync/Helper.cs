750a73eca177e945bb00cd277d6473111463d92f
TrungKhanhNguyen <khanhnt92@gmail.com>
2022-12-27 20:40:31 +0700

    update sync

--- XMSIntellegoSync/Helper.cs
+++ XMSIntellegoSync/Helper.cs
@@ -29,8 +29,8 @@ namespace XMSIntellegoSync
         //I.1.Lấy danh sách các intercept được mới trên Intellego
         public MySqlCommand getInterceptInfo(MySqlConnection connection,string fromDate, string toDate)
         {
-            //string sql = "SELECT intercept.id, intercept.name FROM intellego.intercept where dateCreated between '"+fromDate+"' and '"+toDate+"';";
-            string sql = "SELECT intercept.id, intercept.name FROM intellego.intercept where id = '2621'";
+            string sql = "SELECT intercept.id, intercept.name FROM intellego.intercept where dateCreated between '"+fromDate+"' and '"+toDate+"';";
+            //string sql = "SELECT intercept.id, intercept.name FROM intellego.intercept where id = '2621'";
             var cmd = new MySqlCommand(sql, connection);
             return cmd;
         }
@@ -80,6 +80,63 @@ namespace XMSIntellegoSync
         public MySqlCommand updateProvinceNameAndCaseIntercept(MySqlConnection connection, string provinceName,string intercept, string id)
         {
             string sql = "";
+            var cmd = new MySqlCommand(sql, connection);
+            return cmd;
+        }
+
+        //Update thông tin phần địa phương phần 2.1
+        public MySqlCommand updateInterceptAndCaseIntercept(MySqlConnection connection,string interceptid, string caseid, string provinceName)
+        {
+            string sql = "update intercept set provinceName = '"+provinceName+"' where id = '"+interceptid+"'; INSERT INTO case_intercept (intercept, case_intercept.case) SELECT * FROM (SELECT '"+interceptid+"' AS intercept, '"+caseid+"' AS 'case') AS temp WHERE NOT EXISTS (SELECT intercept, case_intercept.case FROM case_intercept WHERE intercept = '"+interceptid+"' AND case_intercept.case = '"+caseid+"') LIMIT 1;";
+            var cmd = new MySqlCommand(sql, connection);
+            return cmd;
+        }
+
+        public MySqlCommand getListInterceptIdByName(MySqlConnection connection, List<NewSyncObject> listObject)
+        {
+           
+            string listItem = "";
+            foreach (var item in listObject)
+            {
+                listItem += item.CASEID;
+                var itemIndex = listObject.IndexOf(item);
+                if (itemIndex != listObject.Count() - 1)
+                {
+                    listItem += ",";
+                }
+            }
+            string sql = "";
+            if (String.IsNullOrEmpty(listItem))
+            {
+                sql = "select * from intellego.intercept where id = 0";
+            }
+            else
+            {
+                sql = "select id,name from intellego.intercept where name in ( " + listItem + ");";
+            }
+            var cmd = new MySqlCommand(sql, connection);
+            return cmd;
+        }
+
+        public MySqlCommand getInterceptIdByName(MySqlConnection connection, string name)
+        {
+            string sql = "SELECT intercept.id, intercept.name FROM intellego.intercept WHERE intercept.name = '" + name + "'";
+            var cmd = new MySqlCommand(sql, connection);
+            return cmd;
+        }
+
+        //Update trashtime với các Intercept của Địa phương
+        public MySqlCommand updateInterceptTrashTime(MySqlConnection connection,string interceptid)
+        {
+            string sql = "UPDATE intercept a JOIN case_intercept b ON a.id= '"+interceptid+"' AND a.id=b.intercept AND b.case in (273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300) SET a.trashedTime=current_timestamp();";
+            var cmd = new MySqlCommand(sql, connection);
+            return cmd;
+        }
+
+        //Update suspendedtime với các Intercept KHÔNG của Địa phương
+        public MySqlCommand updateInterceptSuspendedTime(MySqlConnection connection, string interceptid)
+        {
+            string sql = "UPDATE intercept a JOIN case_intercept b ON a.id= '"+interceptid+"' AND a.id=b.intercept AND b.case not in (273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300) SET a.suspendedTime=current_timestamp();";
             var cmd = new MySqlCommand(sql, connection);
             return cmd;
         }
--- XMSIntellegoSync/MainJob.cs
+++ XMSIntellegoSync/MainJob.cs
@@ -27,135 +27,228 @@ namespace XMSIntellegoSync
             var startTime = (currentTime).AddHours(-8).ToString("yyyy-MM-dd HH:mm:ss");
             var endTime = (currentTime).AddHours(-7).ToString("yyyy-MM-dd HH:mm:ss");
 
-            getListSyncObject(startTime, endTime);
+            syncNewData(startTime, endTime);
+
+            syncUpdatedData(startTime, endTime);
         }
-        private List<NewSyncObject> getListSyncObject(string startTime, string endTime)
+        private void syncNewData(string startTime, string endTime)
         {
-            string connectionString = helper.getIntellgoConnectionString();
-            string xcpconnectionString = helper.getXCDBConnectionString();
-            //var fromDate = DateTime.Now;
-            var listObject = new List<NewSyncObject>();
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            try
             {
-                connection.Open();
-                using (MySqlCommand cmd = helper.getInterceptInfo(connection,startTime,endTime))
+                string connectionString = helper.getIntellgoConnectionString();
+                string xcpconnectionString = helper.getXCDBConnectionString();
+                //var fromDate = DateTime.Now;
+                var listObject = new List<NewSyncObject>();
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    using (MySqlDataReader rdr = cmd.ExecuteReader())
+                    connection.Open();
+                    using (MySqlCommand cmd = helper.getInterceptInfo(connection, startTime, endTime))
                     {
-                        while (rdr.Read())
+                        using (MySqlDataReader rdr = cmd.ExecuteReader())
                         {
-                            var tempObj = new NewSyncObject
+                            while (rdr.Read())
                             {
-                                InterceptId = rdr.GetString(0),
-                                InterceptName = rdr.GetString(1),
-                            };
-                            listObject.Add(tempObj);
-                        }
-                    }
-                }
-
-                if(listObject.Count() > 0)
-                {
-                    using (MySqlConnection xcpconnection = new MySqlConnection(xcpconnectionString))
-                    {
-                        xcpconnection.Open();
-                        using (MySqlCommand cmd = helper.getListXcipioInfo(xcpconnection, listObject))
-                        {
-                            using (MySqlDataReader rdr = cmd.ExecuteReader())
-                            {
-                                while (rdr.Read())
+                                var tempObj = new NewSyncObject
                                 {
-                                    var caseid = rdr.GetString(0);
-                                    var name = rdr.GetString(1);
-                                    var optionvalue = rdr.GetString(2);
-                                    var stopdatetime = rdr.GetString(3);
-                                    var mod_ts = rdr.GetString(4);
-                                    var state = rdr.GetString(5);
-
-                                    var tempItem = listObject.Where(m => m.InterceptName == caseid).FirstOrDefault();
-                                    if (tempItem != null)
-                                    {
-                                        try
-                                        {
-                                            var tempDate = stopdatetime.Replace('T', ' ').Replace('Z',' '); 
-                                            tempItem.STOPDATETIME = tempDate;
-                                            tempItem.OPTIONVALUE = optionvalue;
-                                            tempItem.NAME = name;
-                                            tempItem.MOD_TS = mod_ts;
-                                            tempItem.STATE = state;
-                                            tempItem.CASEID = caseid;
-                                        }
-                                        catch { }
-                                        
-                                    }
-                                }
+                                    InterceptId = rdr.GetString(0),
+                                    InterceptName = rdr.GetString(1),
+                                };
+                                listObject.Add(tempObj);
                             }
                         }
                     }
 
-                    foreach (var item in listObject)
+                    if (listObject.Count() > 0)
                     {
-                        string sql = "Update intellego.intercept, target SET intercept.expiration_date = '" + item.STOPDATETIME + "' , intercept.description = '" + item.NAME + "', intercept.interceptType = 'CC', target.servid = '" + item.OPTIONVALUE + "' where intercept.name = '" + item.InterceptName + "' and intercept.id='" + item.InterceptId + "';";
-                        var cmd = new MySqlCommand(sql, connection);
-                        cmd.ExecuteNonQuery();
+                        using (MySqlConnection xcpconnection = new MySqlConnection(xcpconnectionString))
+                        {
+                            xcpconnection.Open();
+                            using (MySqlCommand cmd = helper.getListXcipioInfo(xcpconnection, listObject))
+                            {
+                                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                                {
+                                    while (rdr.Read())
+                                    {
+                                        var caseid = rdr.GetString(0);
+                                        var name = rdr.GetString(1);
+                                        var optionvalue = rdr.GetString(2);
+                                        var stopdatetime = rdr.GetString(3);
+                                        var mod_ts = rdr.GetString(4);
+                                        var state = rdr.GetString(5);
+
+                                        var tempItem = listObject.Where(m => m.InterceptName == caseid).FirstOrDefault();
+                                        if (tempItem != null)
+                                        {
+                                            try
+                                            {
+                                                var tempDate = stopdatetime.Replace('T', ' ').Replace('Z', ' ');
+                                                tempItem.STOPDATETIME = tempDate;
+                                                tempItem.OPTIONVALUE = optionvalue;
+                                                tempItem.NAME = name;
+                                                tempItem.MOD_TS = mod_ts;
+                                                tempItem.STATE = state;
+                                                tempItem.CASEID = caseid;
+                                            }
+                                            catch { }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        var listDiaPhuong = db.ExportTargets.Where(m => m.Active == true).ToList();
+
+                        foreach (var item in listObject)
+                        {
+                            string sql = "Update intellego.intercept, target SET intercept.expiration_date = '" + item.STOPDATETIME + "' , intercept.description = '" + item.NAME + "', intercept.interceptType = 'CC', target.servid = '" + item.OPTIONVALUE + "' where intercept.name = '" + item.InterceptName + "' and intercept.id='" + item.InterceptId + "';";
+                            var cmd = new MySqlCommand(sql, connection);
+                            cmd.ExecuteNonQuery();
+
+                            var provinceName = getProvinceNameFromFullName(item.NAME);
+                            var tempDiaphuong = listDiaPhuong.Where(m => m.TargetName == provinceName).FirstOrDefault();
+                            if (tempDiaphuong != null)
+                            {
+                                var caseid = tempDiaphuong.TargetId;
+                                UpdateProvinceData(connection, item.InterceptId, caseid, provinceName);
+                            }
+                        }
+                        //helper.UpdateData(listObject);
+                        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm: ") + "[DONE] Synced "+listObject.Count()+" NEW DATA " + startTime + " to " + endTime );
                     }
-                    //helper.UpdateData(listObject);
                 }
+                //return listObject;
             }
-            return listObject;
+            catch (Exception ex) {
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm: ") + "[FAILED] Error when sync NEW DATA "+ startTime + " to " + endTime + " " + ex.Message);
+            }
+            
         }
 
 
         private void syncUpdatedData(string startTime, string endTime)
         {
-            string connectionString = helper.getIntellgoConnectionString();
-            string xcpconnectionString = helper.getXCDBConnectionString();
-            //var fromDate = DateTime.Now;
-            var listObject = new List<NewSyncObject>();
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            try
             {
-                connection.Open();
-                using (MySqlCommand cmd = helper.getListUpdatedIntercept(connection, startTime, endTime)) {
-                    using (MySqlDataReader rdr = cmd.ExecuteReader())
+                string connectionString = helper.getIntellgoConnectionString();
+                string xcpconnectionString = helper.getXCDBConnectionString();
+                //var fromDate = DateTime.Now;
+                var listObject = new List<NewSyncObject>();
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (MySqlCommand cmd = helper.getListUpdatedIntercept(connection, startTime, endTime))
                     {
-                        while (rdr.Read())
+                        using (MySqlDataReader rdr = cmd.ExecuteReader())
                         {
-                            var caseid = rdr.GetString(0);
-                            var name = rdr.GetString(1);
-                            var optionvalue = rdr.GetString(2);
-                            var stopdatetime = rdr.GetString(3);
-                            var mod_ts = rdr.GetString(4);
-                            var state = rdr.GetString(5);
-                            var tempDate = stopdatetime.Replace('T', ' ').Replace('Z', ' ');
-
-                            var tempObj = new NewSyncObject
+                            while (rdr.Read())
                             {
-                                CASEID = caseid,
-                                NAME = name,
-                               MOD_TS = mod_ts,
-                               STATE = state,
-                               STOPDATETIME = tempDate,
-                               OPTIONVALUE = optionvalue
-                            };
-                            listObject.Add(tempObj);
+                                var caseid = rdr.GetString(0);
+                                var name = rdr.GetString(1);
+                                var optionvalue = rdr.GetString(2);
+                                var stopdatetime = rdr.GetString(3);
+                                var mod_ts = rdr.GetString(4);
+                                var state = rdr.GetString(5);
+                                var tempDate = stopdatetime.Replace('T', ' ').Replace('Z', ' ');
+
+                                var tempObj = new NewSyncObject
+                                {
+                                    CASEID = caseid,
+                                    NAME = name,
+                                    MOD_TS = mod_ts,
+                                    STATE = state,
+                                    STOPDATETIME = tempDate,
+                                    OPTIONVALUE = optionvalue
+                                };
+                                listObject.Add(tempObj);
+                            }
                         }
                     }
-                }
 
-                var listDiaPhuong = db.ExportTargets.Where(m => m.Active == true).ToList();
-                var listActiveObject = listObject.Where(m => m.STATE == "ACTIVE").ToList();
-                foreach(var item in listActiveObject)
-                {
-                    var cmd = helper.updateActiveRecordOnXcipio(connection,item.NAME, item.OPTIONVALUE, item.CASEID);
-                    cmd.ExecuteNonQuery();
+                    //lấy thông tin InterceptId của từng Intercept
+                    using (MySqlCommand command = helper.getListInterceptIdByName(connection, listObject))
+                    {
+                        using (MySqlDataReader rdr = command.ExecuteReader())
+                        {
+                            while (rdr.Read())
+                            {
+                                var tempId = rdr.GetString(0);
+                                var tempName = rdr.GetString(1);
+                                var tempObj = listObject.Where(m => m.CASEID == tempName).FirstOrDefault();
+                                tempObj.InterceptId = tempId;
+                            }
+                        }
+                    }
 
-                    var provinceIndex = item.NAME.IndexOf('_');
-                    var provinceName = item.NAME.Substring(0, provinceIndex);
+                    var listDiaPhuong = db.ExportTargets.Where(m => m.Active == true).ToList();
+                    var listActiveObject = listObject.Where(m => m.STATE == "ACTIVE").ToList();
+                    foreach (var item in listActiveObject)
+                    {
+                        try
+                        {
+                            var cmd = helper.updateActiveRecordOnXcipio(connection, item.NAME, item.OPTIONVALUE, item.CASEID);
+                            cmd.ExecuteNonQuery();
 
+                            var provinceName = getProvinceNameFromFullName(item.NAME);
 
+                            var tempDiaphuong = listDiaPhuong.Where(m => m.TargetName == provinceName).FirstOrDefault();
+                            if (tempDiaphuong != null)
+                            {
+                                var interceptid = item.InterceptId;
+                                var caseid = tempDiaphuong.TargetId;
+                                UpdateProvinceData(connection, interceptid, caseid, provinceName);
+                            }
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm: ") + "[FAILED] Error when sync UPDATE DATA " + startTime + " to " + endTime + " , Active intercept: " + item.CASEID + ex.Message);
+                        }
+                        
+                    }
 
+                    var listInactiveObject = listObject.Where(m => m.STATE == "INACTIVE").ToList();
+                    foreach (var item in listInactiveObject)
+                    {
+                        try
+                        {
+                            var provinceName = getProvinceNameFromFullName(item.NAME);
+                            var tempDiaphuong = listDiaPhuong.Where(m => m.TargetName == provinceName).FirstOrDefault();
+                            if (tempDiaphuong != null)
+                            {
+                                var cmd = helper.updateInterceptTrashTime(connection, item.InterceptId);
+                                cmd.ExecuteNonQuery();
+                            }
+                            else
+                            {
+                                var cmd = helper.updateInterceptSuspendedTime(connection, item.InterceptId);
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm: ") + "[FAILED] Error when sync UPDATE DATA " + startTime + " to " + endTime + " , Inactive intercept: " + item.CASEID + ex.Message);
+                        }
+                        
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm: ") + "[FAILED] Error when sync UPDATE DATA " + startTime + " to " + endTime + " " + ex.Message);
+            }
+            
+        }
+
+        private void UpdateProvinceData(MySqlConnection connection, string interceptid, string caseid, string provinceName)
+        {
+            var cmdUpdate = helper.updateInterceptAndCaseIntercept(connection, interceptid, caseid, provinceName);
+            cmdUpdate.ExecuteNonQuery();
+        }
+
+        private string getProvinceNameFromFullName(string name)
+        {
+            var provinceIndex = name.IndexOf('_');
+            var provinceName = name.Substring(0, provinceIndex);
+            return provinceName;
         }
         
     }
